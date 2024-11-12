5667ece44de8c4d369e4e67b45924db8134e9d0c
Essa Almutiri <easa-01@hotmail.com>
2024-11-12 14:33:44 +0300

    Add files via upload
    Exercise (Introduction to Spring Boot)
    ESSA ALMUTIRI

---
+++ Exercise_IntroductionToSpringBoot/src/main/java/com/example/exercise_introductiontospringboot/Exercise.java
@@ -0,0 +1,51 @@
+package com.example.exercise_introductiontospringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class Exercise {
+
+    @GetMapping("/name")
+    public String name() {
+        return "Essa Almutiri";
+    }
+
+    @GetMapping("/age")
+    public int age() {
+        return 25;
+    }
+
+    @GetMapping("/check/status")
+    public String checkStatus() {
+        return "Everything is OK";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return " “Server health is up and running”";
+    }
+
+
+        int[] arr = new int[10];
+        @GetMapping("/Array")
+        public int[] getArr() {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = i+1;
+            }
+
+//            for (int i = 0; i < arr.length; i++) {
+//                 System.out.println(arr[i]);
+//
+//            }
+            return arr;
+        }
+
+
+
+
+
+
+}
---
+++ Exercise_IntroductionToSpringBoot/src/main/java/com/example/exercise_introductiontospringboot/ExerciseIntroductionToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.exercise_introductiontospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseIntroductionToSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseIntroductionToSpringBootApplication.class, args);
+    }
+
+}
